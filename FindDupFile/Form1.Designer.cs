677df9d3b2fea19f0f3dfcbe1a5954dc9883d16c
zixuany <nekomail@qq.com>
2020-07-03 17:56:39 +0800

    添加项目文件。

---
+++ FindDupFile/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace FindDupFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPaths = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnAddPath = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tlbAction = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPaths
+            // 
+            this.txtPaths.Location = new System.Drawing.Point(12, 12);
+            this.txtPaths.Multiline = true;
+            this.txtPaths.Name = "txtPaths";
+            this.txtPaths.Size = new System.Drawing.Size(776, 107);
+            this.txtPaths.TabIndex = 0;
+            // 
+            // btnAddPath
+            // 
+            this.btnAddPath.Location = new System.Drawing.Point(12, 125);
+            this.btnAddPath.Name = "btnAddPath";
+            this.btnAddPath.Size = new System.Drawing.Size(75, 23);
+            this.btnAddPath.TabIndex = 1;
+            this.btnAddPath.Text = "添加路径";
+            this.btnAddPath.UseVisualStyleBackColor = true;
+            this.btnAddPath.Click += new System.EventHandler(this.btnAddPath_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(93, 125);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "检查重复";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tlbAction});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tlbAction
+            // 
+            this.tlbAction.Name = "tlbAction";
+            this.tlbAction.Size = new System.Drawing.Size(31, 17);
+            this.tlbAction.Text = "就绪";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnAddPath);
+            this.Controls.Add(this.txtPaths);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPaths;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnAddPath;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel tlbAction;
+    }
+}
+
---
+++ FindDupFile/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindDupFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> ScanPaths = new List<string>();
+        private List<ScanFileInfo> ScanFiles = new List<ScanFileInfo>();
+
+        private void btnAddPath_Click(object sender, EventArgs e)
+        {
+            if(this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var selPath = this.folderBrowserDialog1.SelectedPath;
+                if (Directory.Exists(selPath) && !this.ScanPaths.Exists(x => x.ToLower() == selPath.ToLower()))
+                    this.ScanPaths.Add(selPath);
+            }
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                this.ScanFile();
+
+                this.ComparisonFile();
+            });
+        }
+
+        private void ScanFile()
+        {
+            this.ScanFiles.Clear();
+            this.UpdateAction("查找文件中...");
+        
+            List<string> filePaths = new List<string>();
+            foreach (var path in this.ScanPaths)
+            {
+                var filepaths = Directory
+                    .GetFiles(path, "*.cs", SearchOption.AllDirectories);
+
+                foreach (var fp in filepaths)
+                {
+                    this.ScanFiles.Add(new ScanFileInfo()
+                    {
+                        Path = fp
+                    });
+                }
+            }
+
+            this.UpdateAction($"共查找{ScanFiles.Count}个文件");
+        }
+
+        private void ComparisonFile()
+        {
+            this.UpdateAction("開始生成MD5...");
+
+            this.RunTask(this.ScanFiles, new Func<ScanFileInfo, ScanFileInfo>(x =>
+             {
+                 var handlCount = this.ScanFiles.Count(y => !string.IsNullOrEmpty(y.MD5));
+
+                 x.MD5 = GetMD5HashFromFile(x.Path);
+                 this.UpdateAction($"生成MD5[{handlCount}/{this.ScanFiles.Count}]");
+                 return x;
+             }), 3);
+        }
+
+        private void UpdateAction(string info)
+        {
+            this.Invoke(new Action(() => 
+            {
+                this.tlbAction.Text = info;
+            }));
+        }
+
+        /// <summary>
+        /// 多线程处理数据（返回处理后列表）
+        /// </summary>
+        /// <typeparam name="T">数据类型</typeparam>
+        /// <param name="list">待处理数据</param>
+        /// <param name="func">数据处理方法（有参数有返回值）</param>
+        /// <param name="threadCount">处理线程数量</param>
+        /// <returns>数据处理后结果</returns>
+        private List<T> RunTask<T>(List<T> list, Func<T, T> func, int threadCount = 5)
+        {
+            var result = new List<T>();
+            ConcurrentQueue<T> queue = new ConcurrentQueue<T>(list);
+            Task<List<T>>[] tasks = new Task<List<T>>[threadCount];
+            for (int i = 0; i < threadCount; i++)
+            {
+                tasks[i] = Task.Run<List<T>>(() =>
+                {
+                    var rList = new List<T>();
+                    while (queue.TryDequeue(out T t))
+                    {
+                        rList.Add(func(t));
+                    }
+                    return rList;
+                });
+            }
+            Task.WaitAll(tasks);
+            for (int i = 0; i < threadCount; i++)
+            {
+                result.AddRange(tasks[i].Result);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 获取文件MD5值
+        /// </summary>
+        /// <param name="fileName">文件绝对路径</param>
+        /// <returns>MD5值</returns>
+        public static string GetMD5HashFromFile(string fileName)
+        {
+            try
+            {
+                FileStream file = new FileStream(fileName, FileMode.Open);
+                System.Security.Cryptography.MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+                byte[] retVal = md5.ComputeHash(file);
+                file.Close();
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < retVal.Length; i++)
+                {
+                    sb.Append(retVal[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("GetMD5HashFromFile() fail,error:" + ex.Message);
+            }
+        }
+    }
+}
